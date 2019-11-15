799d75747b5b321c1486cd63b6d0d80453b97bfc
unknown <2146124@labinfo.is.escuelaing.edu.co>
2019-11-15 14:14:41 -0500

    Arreglo de las clases

--- src/main/java/edu/eci/cvds/persistence/mybatisimpl/MyBatisDaoReserva.java
+++ src/main/java/edu/eci/cvds/persistence/mybatisimpl/MyBatisDaoReserva.java
@@ -1,10 +1,11 @@
 package edu.eci.cvds.persistence.mybatisimpl;
 
 import edu.eci.cvds.entities.Recurso;
+import edu.eci.cvds.entities.Reserva;
 import edu.eci.cvds.entities.Usuario;
 
 import edu.eci.cvds.persistence.DaoReserva;
-import edu.eci.cvds.persistence.mybatisimpl.mappers.RecursoRentadoMapper;
+import edu.eci.cvds.persistence.mybatisimpl.mappers.ReservaMapper;
 
 import javax.inject.Inject;
 import java.sql.Timestamp;
@@ -12,12 +13,12 @@ import java.sql.Timestamp;
 public class MyBatisDaoReserva implements DaoReserva {
 
     @Inject
-    private RecursoRentadoMapper recursoRentadoMapper;
+    private ReservaMapper reservaMapper;
 
     @Override
     public void reservarRecurso(Recurso recurso, Usuario usuario) {
         Timestamp fechaIni=new Timestamp(System.currentTimeMillis());
         Timestamp fechaFin=new Timestamp(System.currentTimeMillis());
-        recursoRentadoMapper.reservarRecurso(recurso,usuario,fechaIni,fechaFin);
+        reservaMapper.reservarRecurso(recurso,usuario,fechaIni,fechaFin);
     }
 }
